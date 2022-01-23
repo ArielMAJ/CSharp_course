c95a193e4f40d76c1cee18de5c994edba192528c
ArielMAJ <ariel.maj@hotmail.com>
2022-01-23 20:53:42 -0300

    Aula 121 completa

--- Aula_120_e_121/Entities/Worker.cs
+++ Aula_120_e_121/Entities/Worker.cs
@@ -5,5 +5,42 @@ namespace Course.Entities
     class Worker
     {
         public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+    
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Month != month || contract.Date.Year != year) continue;
+                sum += contract.TotalValue();
+            }
+            return sum;
+        }
     }
 }
--- Aula_120_e_121/Program.cs
+++ Aula_120_e_121/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Course.Entities;
+using Course.Entities.Enums;
+using System.Globalization;
 
 namespace Course
 {
@@ -6,7 +9,43 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi");
+            Console.Write("Enter department's name: ");
+            string deptName = Console.ReadLine();
+            Console.Write("Name: ");
+            string Name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior): ");
+            WorkerLevel level = Enum.Parse<WorkerLevel>(Console.ReadLine());
+            Console.Write("Base salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Department dept = new Department(deptName);
+            Worker worker = new Worker(Name, level, baseSalary, dept);
+
+            Console.Write("How many contracts to this worker? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter #{i + 1} contract data: ");
+                Console.Write("Data (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHours = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hour): ");
+                int hours = int.Parse(Console.ReadLine());
+                HourContract contract = new HourContract(date, valuePerHours, hours);
+                worker.AddContract(contract);
+            }
+            Console.WriteLine();
+            Console.Write("Enter month and year to calculate income (MM/YYYY): ");
+            string monthAndYear = Console.ReadLine();
+            int month = int.Parse(monthAndYear.Substring(0, 2));
+            int year = int.Parse(monthAndYear.Substring(3));
+            Console.WriteLine("Name: " + worker.Name);
+            Console.WriteLine("Department: " + worker.Department.Name);
+            Console.WriteLine($"Income for {monthAndYear}: " + worker.Income(year, month).ToString("F2", CultureInfo.InvariantCulture));
+
+
         }
     }
 }
