bac182147829723d76361861c382e6dc477fb247
ArielMAJ <ariel.maj@hotmail.com>
2022-01-31 22:51:26 -0300

    Aula 126

---
+++ Aula_126/Entities/BusinessAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace Aula_126.Entities
+{
+    internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() : base()
+        {
+
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) 
+            : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+                Balance += amount;
+        }
+    }
+}
---
+++ Aula_126/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Aula_126.Entities;
+namespace Aula_126
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(8010, "Bob Meown", 100.0, 500.0);
+            Console.WriteLine(account.Balance);
+
+        }
+    }
+}
